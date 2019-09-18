52eceb6b47776549b21e02aab626266813239385
Guillaume <guillaume.tranain@gmail.com>
2019-09-18 10:18:06 +0200

    :boom: :art: :zap:
    Réorganisation de la structure du projet afin de pouvoir le builder et le mettre sur GitHub Pages via Travis-CI

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to CV-ng-material!');
+    expect(page.getTitleText()).toEqual('Welcome to cv!');
   });
 
   afterEach(async () => {
